d67b04ce9449ab6e4e1857267e54ff433746d26c
patheeb <pat.heeb@kellogg.northwestern.edu>
2022-06-03 04:29:33 +0000

    hw4 update 6.2.22

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,14 @@
 class PlacesController < ApplicationController
 
+
+
   def index
     @places = Place.all
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["user_id"]  })
   end
 
   def new
@@ -19,5 +21,6 @@ class PlacesController < ApplicationController
     @place.save
     redirect_to "/places"
   end
+
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,31 @@
 class SessionsController < ApplicationController
   def new
-  end
+  end 
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome Back"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Error! Username or password incorrect"
+        redirect_to "/sessions"
+      end
+    else
+      flash["notice"] = "Error! Username or password incorrect"
+      redirect_to "/sessions"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/sessions"
   end
+
+
+
 end
   
